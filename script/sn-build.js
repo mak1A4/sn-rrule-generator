c59eb8bb28d96fbbdc43c453fe23e7be7a9af9ed
mak1A4 <m.kirchweger@protonmail.com>
2021-07-25 01:42:07 +0200

    bump

--- script/sn-build.js
+++ script/sn-build.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
+const YAML = require('yaml');
 const cheerio = require('cheerio');
+const configObj = YAML.parse(fs.readFileSync('./sn.deploy.config.yml', 'utf-8'));
+const apiPath = configObj.apiPath + configObj.uiPageName;
 
 var indexHtml = fs.readFileSync("./dist/index.html", "utf-8");
-var $ = cheerio.load(indexHtml, {
-    xmlMode: true
-});
+var $ = cheerio.load(indexHtml, {}, false);
 
 $("link").each((i, el) => {
     var hrefStr = $(el).attr("href");
-    $(el).attr("href", "/api/swp?file=" + hrefStr.substr(1));
+    $(el).attr("href", apiPath + "?file=" + hrefStr.substr(1));
 });
 $("script").each((i, el) => {
     var srcStr = $(el).attr("src");
-    $(el).attr("src", "/api/swp?file=" + srcStr.substr(1));
+    $(el).attr("src", apiPath + "?file=" + srcStr.substr(1));
 });
 
 var finalHtmlStr = $.html();
+console.log(finalHtmlStr);
 var uiPage = fs.readFileSync("./src/ui_page.xml");
-$ = cheerio.load(uiPage, {
-    xmlMode: true
-});
+$ = cheerio.load(uiPage, {}, false);
 $("#app").replaceWith(finalHtmlStr);
 var finalUiPageStr = $.html();
 
