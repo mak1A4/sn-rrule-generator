0f0a59500ceeedb3a08e4ef723472963cc04c023
mak1A4 <m.kirchweger@protonmail.com>
2021-07-26 10:49:51 +0200

    working build + deploy

--- script/sn-build.js
+++ script/sn-build.js
@@ -7,9 +7,11 @@ const apiPath = configObj.apiPath + configObj.uiPageName;
 var indexHtml = fs.readFileSync("./dist/index.html", "utf-8");
 var $ = cheerio.load(indexHtml, {}, false);
 
+var linkList = [];
 $("link").each((i, el) => {
     var hrefStr = $(el).attr("href");
     $(el).attr("href", apiPath + "?file=" + hrefStr.substr(1));
+    linkList.push($(el).toString());
 });
 $("script").each((i, el) => {
     var srcStr = $(el).attr("src");
@@ -17,10 +19,21 @@ $("script").each((i, el) => {
 });
 
 var finalHtmlStr = $.html();
-console.log(finalHtmlStr);
 var uiPage = fs.readFileSync("./src/ui_page.xml");
 $ = cheerio.load(uiPage, {}, false);
+
+var inputList = [];
+$("input").each((i, el) => {
+    inputList.push($(el).toString());
+});
+
 $("#app").replaceWith(finalHtmlStr);
 var finalUiPageStr = $.html();
+inputList.forEach((input) => {
+    finalUiPageStr = finalUiPageStr.replace(input, input + "</input>");
+});
+linkList.forEach((lnk) => {
+    finalUiPageStr = finalUiPageStr.replace(lnk, lnk + "</link>");
+});
 
 fs.writeFileSync("./dist/ui_page.xml", finalUiPageStr);
--- script/sn-deploy.js
+++ script/sn-deploy.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const YAML = require('yaml');
+const mime = require("mime-types");
 const oauth = require('sn-oauth').default;
 const axios = require('axios').default;
 const configObj = YAML.parse(fs.readFileSync('./sn.config.yml', 'utf-8'));
@@ -26,6 +27,81 @@ async function getUiPageRecord(name, authHeader) {
   }
 }
 
+async function deployUiPage(method, name, authHeader, uiPageSysId) {
+  try {
+    let postUrl = `${tableApiUrl}/sys_ui_page`;
+    if (method == "PUT" && uiPageSysId) {
+      postUrl += "/" + uiPageSysId;
+    }
+    let response = await axios({
+      "method": method,
+      "url": postUrl,
+      "data": JSON.stringify({
+        "name": name,
+        "direct": true,
+        "html": fs.readFileSync("./dist/ui_page.xml", "utf-8")
+      }),
+      "headers": {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": authHeader["Authorization"]
+      }
+    });
+    return {
+      "result": response.data.result,
+      "status": response.status
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deployUiPageAttachments(uiPageSysId, authHeader) {
+  let attachmentApiUrl = `${instanceUrl}/api/now/attachment`;
+  let getUrl = `${attachmentApiUrl}?sysparm_query=table_name=sys_ui_page^table_sys_id=${uiPageSysId}`
+  let attachmentResponse = await axios.get(getUrl, { headers: authHeader });
+  for (const attm of attachmentResponse.data.result) {
+    await axios({
+      "method": "DELETE",
+      "url": `${attachmentApiUrl}/${attm.sys_id}`,
+      "headers": authHeader
+    });
+  }
+
+  var fileList = fs.readdirSync("./dist", "utf-8").filter((f) => {
+    if (f.endsWith(".js") || f.endsWith(".css")) return true;
+    return false;
+  }).map((f) => {
+    var fpath = `./dist/${f}`;
+    return {
+      "name": f,
+      "contentType": mime.lookup(fpath),
+      "content": fs.readFileSync(fpath, "utf-8")
+    };
+  });
+
+  var result = [];
+  for (const fileObj of fileList) {
+    try {
+      var postUrl = `${attachmentApiUrl}/file?table_name=sys_ui_page&table_sys_id=${uiPageSysId}&file_name=${fileObj.name}`;
+      var insertResponse = await axios({
+        "method": "POST",
+        "url": postUrl,
+        "data": Buffer.from(fileObj.content, "utf-8"),
+        "headers": {
+          "Accept": "application/json",
+          "Content-Type": fileObj.contentType,
+          "Authorization": authHeader["Authorization"]
+        }
+      });
+      result.push(insertResponse.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return result;
+}
+
 (async function () {
   if (!fs.existsSync(tokenPath)) {
     let tokenObj = await oauth();
@@ -41,6 +117,13 @@ async function getUiPageRecord(name, authHeader) {
     tokenObj = await oauth(tokenObj.instanceName, tokenObj.clientId, tokenObj.clientSecret, tokenObj.refreshToken);
     accessToken = tokenObj.accessToken;
     fs.writeFileSync(tokenPath, JSON.stringify(tokenObj));
+    uiPageResult = await getUiPageRecord(configObj.uiPageName, authHeader);
   }
-  console.log(uiPageResult);
+  if (!uiPageResult.result) {
+    uiPageResult = await deployUiPage("POST", configObj.uiPageName, authHeader);
+  } else {
+    uiPageResult = await deployUiPage("PUT", configObj.uiPageName, authHeader, uiPageResult.result.sys_id);
+  }
+  var attmResult = await deployUiPageAttachments(uiPageResult.result.sys_id, authHeader);
+  console.log("Finished with deployment of UI-Page and " + attmResult.length + " attachments.");
 })();
