00d116246dd8a1c0d8125c89a42e0cf82bbe90ce
mak1A4 <m.kirchweger@protonmail.com>
2021-07-26 15:19:19 +0200

    publish now also works with MFA

--- script/sn-deploy.js
+++ script/sn-deploy.js
@@ -5,15 +5,7 @@ const oauth = require('sn-oauth').default;
 const axios = require('axios').default;
 const dotenv_stringify = require('dotenv-stringify');
 const configObj = YAML.parse(fs.readFileSync('./sn.config.yml', 'utf-8'));
-
 const dotEnvPath = './.env';
-var accessToken, instanceUrl, tableApiUrl;
-if (fs.existsSync(dotEnvPath)) {
-  require('dotenv').config();
-  instanceUrl = "https://" + process.env.INSTANCE_NAME + ".service-now.com";
-  tableApiUrl = `${instanceUrl}/api/now/table`;
-  accessToken = process.env.ACCESS_TOKEN;
-}
 
 function writeDotEnv(obj) {
   fs.writeFileSync(dotEnvPath, dotenv_stringify({
@@ -24,96 +16,113 @@ function writeDotEnv(obj) {
     "REFRESH_TOKEN": obj.refreshToken
   }));
 }
-
-async function getUiPageRecord(name, authHeader) {
-  try {
-    let getUrl = `${tableApiUrl}/sys_ui_page?sysparm_query=name=${name}`;
-    let response = await axios.get(getUrl, { headers: authHeader });
-    return {
-      "result": response.data.result[0],
-      "status": response.status
-    };
-  } catch (err) {
-    if (err.response.status) return { "status": 401 };
-  }
-}
-
-async function deployUiPage(method, name, authHeader, uiPageSysId) {
-  try {
-    let postUrl = `${tableApiUrl}/sys_ui_page`;
-    if (method == "PUT" && uiPageSysId) {
-      postUrl += "/" + uiPageSysId;
+(async function () {
+  var accessToken, instanceUrl, tableApiUrl;
+  if (fs.existsSync(dotEnvPath)) {
+    require('dotenv').config();
+    instanceUrl = "https://" + process.env.INSTANCE_NAME + ".service-now.com";
+    tableApiUrl = `${instanceUrl}/api/now/table`;
+    if (process.env.ACCESS_TOKEN) {
+      accessToken = process.env.ACCESS_TOKEN;
+    } else {
+      let tokenObj = await oauth(
+        process.env.INSTANCE_NAME,
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET
+      );
+      accessToken = tokenObj.accessToken;
+      writeDotEnv(tokenObj);
     }
-    let response = await axios({
-      "method": method,
-      "url": postUrl,
-      "data": JSON.stringify({
-        "name": name,
-        "direct": true,
-        "html": fs.readFileSync("./dist/ui_page.xml", "utf-8")
-      }),
-      "headers": {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": authHeader["Authorization"]
-      }
-    });
-    return {
-      "result": response.data.result,
-      "status": response.status
-    };
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function deployUiPageAttachments(uiPageSysId, authHeader) {
-  let attachmentApiUrl = `${instanceUrl}/api/now/attachment`;
-  let getUrl = `${attachmentApiUrl}?sysparm_query=table_name=sys_ui_page^table_sys_id=${uiPageSysId}`
-  let attachmentResponse = await axios.get(getUrl, { headers: authHeader });
-  for (const attm of attachmentResponse.data.result) {
-    await axios({
-      "method": "DELETE",
-      "url": `${attachmentApiUrl}/${attm.sys_id}`,
-      "headers": authHeader
-    });
   }
 
-  var fileList = fs.readdirSync("./dist", "utf-8").filter((f) => {
-    if (f.endsWith(".js") || f.endsWith(".css")) return true;
-    return false;
-  }).map((f) => {
-    var fpath = `./dist/${f}`;
-    return {
-      "name": f,
-      "contentType": mime.lookup(fpath),
-      "content": fs.readFileSync(fpath, "utf-8")
-    };
-  });
-
-  var result = [];
-  for (const fileObj of fileList) {
+  async function getUiPageRecord(name, authHeader) {
     try {
-      var postUrl = `${attachmentApiUrl}/file?table_name=sys_ui_page&table_sys_id=${uiPageSysId}&file_name=${fileObj.name}`;
-      var insertResponse = await axios({
-        "method": "POST",
+      let getUrl = `${tableApiUrl}/sys_ui_page?sysparm_query=name=${name}`;
+      let response = await axios.get(getUrl, { headers: authHeader });
+      return {
+        "result": response.data.result[0],
+        "status": response.status
+      };
+    } catch (err) {
+      if (err.response.status) return { "status": 401 };
+    }
+  }
+
+  async function deployUiPage(method, name, authHeader, uiPageSysId) {
+    try {
+      let postUrl = `${tableApiUrl}/sys_ui_page`;
+      if (method == "PUT" && uiPageSysId) {
+        postUrl += "/" + uiPageSysId;
+      }
+      let response = await axios({
+        "method": method,
         "url": postUrl,
-        "data": Buffer.from(fileObj.content, "utf-8"),
+        "data": JSON.stringify({
+          "name": name,
+          "direct": true,
+          "html": fs.readFileSync("./dist/ui_page.xml", "utf-8")
+        }),
         "headers": {
           "Accept": "application/json",
-          "Content-Type": fileObj.contentType,
+          "Content-Type": "application/json",
           "Authorization": authHeader["Authorization"]
         }
       });
-      result.push(insertResponse.data);
+      return {
+        "result": response.data.result,
+        "status": response.status
+      };
     } catch (err) {
       console.log(err);
     }
   }
-  return result;
-}
 
-(async function () {
+  async function deployUiPageAttachments(uiPageSysId, authHeader) {
+    let attachmentApiUrl = `${instanceUrl}/api/now/attachment`;
+    let getUrl = `${attachmentApiUrl}?sysparm_query=table_name=sys_ui_page^table_sys_id=${uiPageSysId}`
+    let attachmentResponse = await axios.get(getUrl, { headers: authHeader });
+    for (const attm of attachmentResponse.data.result) {
+      await axios({
+        "method": "DELETE",
+        "url": `${attachmentApiUrl}/${attm.sys_id}`,
+        "headers": authHeader
+      });
+    }
+
+    var fileList = fs.readdirSync("./dist", "utf-8").filter((f) => {
+      if (f.endsWith(".js") || f.endsWith(".css")) return true;
+      return false;
+    }).map((f) => {
+      var fpath = `./dist/${f}`;
+      return {
+        "name": f,
+        "contentType": mime.lookup(fpath),
+        "content": fs.readFileSync(fpath, "utf-8")
+      };
+    });
+
+    var result = [];
+    for (const fileObj of fileList) {
+      try {
+        var postUrl = `${attachmentApiUrl}/file?table_name=sys_ui_page&table_sys_id=${uiPageSysId}&file_name=${fileObj.name}`;
+        var insertResponse = await axios({
+          "method": "POST",
+          "url": postUrl,
+          "data": Buffer.from(fileObj.content, "utf-8"),
+          "headers": {
+            "Accept": "application/json",
+            "Content-Type": fileObj.contentType,
+            "Authorization": authHeader["Authorization"]
+          }
+        });
+        result.push(insertResponse.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    return result;
+  }
+
   if (!fs.existsSync(dotEnvPath)) {
     let tokenObj = await oauth();
     instanceUrl = "https://" + tokenObj.instanceName + ".service-now.com";
